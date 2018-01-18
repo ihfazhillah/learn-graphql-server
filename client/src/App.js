250720513bc1a343f91b8b08587d500a1aa126c9
ihfazhillah <mihfazhillah@gmail.com>
2018-01-19 06:28:09 +0700

    display data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" id="username" />
+          <input type="password" id="password" />
+          <input type="submit" value="login" />
+        </form>
+        <button id="social">Social Login</button>
+      </div>
+    );
+  }
+}
+
+class DataFromGql extends Component {
+  render() {
+    return (
+      <div style={{ margin: 20 }}>
+        <div className="App-title">{this.props.title}</div>
+        <pre style={{ textAlign: "initial", width: "80%", margin: "0 auto" }}>
+          {this.props.content}
+        </pre>
+      </div>
+    );
+  }
+}
+
+const OpenData = graphql(
+  gql`
+    {
+      hello
+    }
+  `,
+  {
+    props: ({ data }) => {
+      data.content = JSON.stringify(data, null, 4);
+      data.title = "open data";
+      return data;
+    }
+  }
+)(DataFromGql);
+
+const CloseData = graphql(
+  gql`
+    {
+      allUsers {
+        id
+        username
+      }
+    }
+  `,
+  {
+    props: ({ data }) => {
+      data.content = JSON.stringify(data, null, 4);
+      data.title = "login required";
+      return data;
+    }
+  }
+)(DataFromGql);
 
 class App extends Component {
   render() {
@@ -13,6 +76,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <LoginForm />
+        <OpenData />
+        <CloseData />
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "./apollo";
+import client from "./apollo";
+import { ApolloProvider } from "react-apollo";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
